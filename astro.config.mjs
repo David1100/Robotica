8633bdf9235100f7c754a0422de79eaaf10d1c1b
David Acevedo <acevdo22@hotmail.com>
2025-08-27 12:32:44 -0500

    Subiendo actual

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 import react from "@astrojs/react";
 
 import icon from 'astro-icon';
-
+import path from 'path';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    }
   }
 });
